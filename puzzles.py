e403c005dbe9b7619a391ce9562a0f93c604062f
firstgenius <yaroslav.brovchenko@ucu.edu.ua>
2021-02-04 21:50:41 +0200

    adding all_in_row

--- puzzles.py
+++ puzzles.py
@@ -41,3 +41,32 @@ def vertical_check(board):
                 if board[j][i] == board[m][i] and j != m and board[j][i] != '*' and board[j][i] != ' ':
                     return False
     return True
+
+
+def all_in_row(board):
+    '''
+    Creates a dictionary in which writes values ​​from corner to line
+
+    >>> all_in_row(["**** ****", "***1 ****", "**  3****", "* 4 1****", "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    {0: '****  3    2  ****', 1: '***  6   8  2***', 2: '** 4     1  **', 3: '*1     83  *', 4: '  31  9 5 '}
+    >>> all_in_row(["987654321", "123456789", "**  3****", "123456789", "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    {0: '91*1  3    2  ****', 1: '82*2 6   8  2***', 2: '73 3     1  **', 3: '64 4   83  *', 4: '5535  9 5 '}
+    '''
+
+    res = {}
+    k = 0
+    for i in range(5):
+        summa = ''
+        for j in range(9 - k):
+            summa += board[j][i]
+        res[i] = summa
+        k += 1
+
+    k = 0
+    helping_counter = 0
+    for i in range(8,3,-1):
+        res[i - 8 + k + helping_counter] += board[i][k:]
+        k += 1
+        helping_counter += 1
+
+    return res
